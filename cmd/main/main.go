0bec64501d3c0edb2f2d84a2e47f4bb0c3a31c20
emad <assimad8@gmail.com>
2024-09-04 17:09:21 +0100

    the tuple datastructure added

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,4 +6,5 @@ import (
 
 func main() {
 	linear.MyList()
+	linear.MyTuple()
 }
--- pkg/datastructure/linear/tuples.go
+++ pkg/datastructure/linear/tuples.go
@@ -1,1 +1,18 @@
 package linear
+
+import (
+	"fmt"
+)
+
+//gets the power series of integer a and returns tuple of square of a
+// and cube of a
+func powerSeries(a int) (int,int) {
+	return a*a, a*a*a
+  }
+
+func MyTuple() {
+	var square int
+	var cube int
+	square,cube = powerSeries(3)
+	fmt.Printf("Square %d\nCube %d\n",square,cube)
+}
