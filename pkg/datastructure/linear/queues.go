f8f8ce79663c649ce096113722d6ff99bb15c3ef
emad <assimad8@gmail.com>
2024-09-08 05:32:01 +0100

    add the queue datastructure with the New and Add methods

--- pkg/datastructure/linear/queues.go
+++ pkg/datastructure/linear/queues.go
@@ -1,1 +1,35 @@
 package linear
+
+type Queue []*Order
+
+type Order struct {
+	priority int
+	quantity int
+	product string
+	customerName string
+}
+// New method
+func (order *Order) New(priority,quantity int,product,customerName string) {
+	order.priority = priority
+	order.quantity = quantity
+	order.product = product
+	order.customerName = customerName
+}
+// Add method
+func (queue *Queue) Add(order *Order) {
+	if len(*queue)==0{
+		*queue = append(*queue, order)
+	}else {
+		appended := false
+		for i,addedOrder := range *queue{
+			if order.priority > addedOrder.priority {
+				*queue = append((*queue)[:i], append(Queue{order}, (*queue)[i:]...)...)
+				appended = true
+				break
+			}
+		}
+		if !appended {
+			*queue = append(*queue,order)
+		}
+	}
+}
