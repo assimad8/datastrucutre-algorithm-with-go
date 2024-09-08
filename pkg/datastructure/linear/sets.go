c98e51d93ab02ac4f1014d334796d67c379100a3
emad <assimad8@gmail.com>
2024-09-08 03:40:03 +0100

    add the set datastructure with intersect and union methods

--- pkg/datastructure/linear/sets.go
+++ pkg/datastructure/linear/sets.go
@@ -26,4 +26,26 @@ func (set *Set) AddElement(e int) {
 func (set *Set) DeleteElement(element int) {
     delete(set.integerMap,element)
 }
-
+// Intersect method 
+func (set *Set) Intersect(otherSet *Set) *Set {
+	intersectSet := new(Set)
+	intersectSet.New()
+	for value,_:=range set.integerMap {
+		if otherSet.ContainsElement(value) {
+			intersectSet.AddElement(value)
+		}
+	}
+	return intersectSet
+}
+// Union method 
+func (set *Set) Union(otherSet *Set) *Set {
+	unionSet := new(Set)
+	unionSet.New()
+	for value,_:=range set.integerMap {
+		unionSet.AddElement(value)
+	}
+	for value,_:=range otherSet.integerMap {
+		unionSet.AddElement(value)
+	}
+	return unionSet
+}
